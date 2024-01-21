fbea69b0f63bce9b7fac037b16f69d46f4fc3b42
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-01-21 20:09:45 +0100

    Listing all available backends

--- wgpu_test/src/main.rs
+++ wgpu_test/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let instance = wgpu::Instance::default();
+    println!("Available backends:");
+    for a in instance.enumerate_adapters(wgpu::Backends::all()) {
+        println!("    {:?}", a.get_info());
+    }
 }
