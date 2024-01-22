d2fc2c8b5f9f7e2be7931c132ea5014e7962b0b8
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-01-22 04:59:38 +0100

    Started testing by creating a Device

--- wgpu_test/src/main.rs
+++ wgpu_test/src/main.rs
@@ -1,7 +1,61 @@
 fn main() {
+    // TODO: initiate logger instead of print
     let instance = wgpu::Instance::default();
     println!("Available backends:");
     for a in instance.enumerate_adapters(wgpu::Backends::all()) {
-        println!("    {:?}", a.get_info());
+        println!("{:?}", a.get_info());
     }
+
+    let d = WgpuDevice::new().expect("Failed to create device");
+    let b = d.create_buffer(20, wgpu::BufferUsages::STORAGE, None);
+    d.que.write_buffer(&b, 0, &[0x01; 20]);
+    println!("Finished");
+}
+
+type DynamicErr = Box<dyn std::error::Error>;
+
+pub struct WgpuDevice {
+    dev: wgpu::Device,
+    que: wgpu::Queue,
+}
+
+impl WgpuDevice {
+    pub fn new() -> Result<Self, DynamicErr> {
+        let rt = tokio::runtime::Runtime::new()?;
+        let (dev, que) = rt.block_on(Self::get_device())?;
+        Ok(Self { dev, que })
+    }
+
+    async fn get_device() -> Result<(wgpu::Device, wgpu::Queue), wgpu::RequestDeviceError> {
+        wgpu::Instance::default()
+            .request_adapter(&wgpu::RequestAdapterOptionsBase { 
+                power_preference: wgpu::PowerPreference::HighPerformance, 
+                force_fallback_adapter: false, 
+                compatible_surface: None, 
+            })
+            .await
+            .expect("No adapter matching preferences")
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::downlevel_defaults(),
+                },
+                None
+            )
+            .await
+    }
+
+    pub fn create_buffer(&self, size: usize, usage: wgpu::BufferUsages, label: Option<&str>) -> wgpu::Buffer {
+        self.dev.create_buffer(&wgpu::BufferDescriptor{
+            size: byte_size(size) as wgpu::BufferAddress,
+            usage,
+            mapped_at_creation: false,
+            label,
+        })
+    }
+}
+
+fn byte_size(size: usize) -> u64 {
+    u64::try_from(size * std::mem::size_of::<u64>()).unwrap()
 }
