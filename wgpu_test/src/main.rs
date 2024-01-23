d17e5803049d619339d2e696142f2c6d5c92160e
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-01-23 09:44:47 +0100

    Checks what devices exists

--- wgpu_test/src/main.rs
+++ wgpu_test/src/main.rs
@@ -1,15 +1,25 @@
-fn main() {
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
     // TODO: initiate logger instead of print
     let instance = wgpu::Instance::default();
     println!("Available backends:");
     for a in instance.enumerate_adapters(wgpu::Backends::all()) {
         println!("{:?}", a.get_info());
+        println!("{:?}",a.request_device(
+            &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::downlevel_defaults(),
+            },
+            None
+        ).await.unwrap().0.global_id());
     }
-
     let d = WgpuDevice::new().expect("Failed to create device");
-    let b = d.create_buffer(20, wgpu::BufferUsages::STORAGE, None);
+    println!("Choosen device: {:?}", d.dev);
+    let b = d.create_buffer(20);
     d.que.write_buffer(&b, 0, &[0x01; 20]);
     println!("Finished");
+    Ok(())
 }
 
 pub struct WgpuDevice {
@@ -32,28 +42,30 @@ impl WgpuDevice {
                 compatible_surface: None, 
             })
             .await
-            .expect("No adapter matching preferences")
+            .expect("No matching adapter for preferences")
             .request_device(
                 &wgpu::DeviceDescriptor {
-                    label: None,
-                    required_features: wgpu::Features::empty(),
-                    required_limits: wgpu::Limits::downlevel_defaults(),
+                        label: None,
+                        required_features: wgpu::Features::empty(),
+                        required_limits: wgpu::Limits::downlevel_defaults(),
                 },
                 None
             )
             .await
     }
 
-    pub fn create_buffer(&self, size: usize, usage: wgpu::BufferUsages, label: Option<&str>) -> wgpu::Buffer {
+    pub fn create_buffer(&self, size: usize) -> wgpu::Buffer {
         self.dev.create_buffer(&wgpu::BufferDescriptor{
-            size: byte_size(size) as wgpu::BufferAddress,
-            usage,
             mapped_at_creation: false,
-            label,
+            label: Some("data buffer"),
+            size: Self::byte_size(size),
+            usage: wgpu::BufferUsages::STORAGE
+                | wgpu::BufferUsages::COPY_DST
+                | wgpu::BufferUsages::COPY_SRC,
         })
     }
-}
 
-fn byte_size(size: usize) -> u64 {
-    u64::try_from(size * std::mem::size_of::<u64>()).unwrap()
+    fn byte_size(size: usize) -> wgpu::BufferAddress {
+        u64::try_from(size * std::mem::size_of::<u64>()).unwrap()
+    }
 }
