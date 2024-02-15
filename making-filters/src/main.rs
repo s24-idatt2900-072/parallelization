a9da0d1613527e0ab9c79e7000d9f33520bf3c20
Vetle Ålesve Nordang <vetlean@stud.ntnu.no>
2024-02-15 11:34:00 +0100

    create filters in rust with python modules and represent them with images

--- making-filters/src/main.rs
+++ making-filters/src/main.rs
@@ -2,51 +2,97 @@ use std::vec;
 
 use pyo3::prelude::*;
 use pyo3::types::PyList;
+use array2d::Array2D;
+use image::{ImageBuffer, Luma};
+
 
 #[derive(Debug)]
 pub struct Filter {
-    filter: Vec<Vec<f64>>,
-    shape: (usize, usize),
+    filter_real: Array2D<f64>,
+    filter_imag: Array2D<f64>,
 }
 
 impl Filter {
-    pub fn new(filter: Vec<Vec<f64>>) -> Self {
-        let shape = (filter.len() , filter[0].len());
-        Self {  filter,
-                shape,
-             }
+    pub fn new(rows_real: Vec<Vec<f64>>, rows_imag: Vec<Vec<f64>>) -> Self {
+        let filter_real = Array2D::from_rows(&rows_real).expect("REASON"); // fikse feil håndtering her
+        let filter_imag = Array2D::from_rows(&rows_imag).expect("REASON"); // fikse feil håndtering her
+        Self { filter_real,
+            filter_imag,
+        }
     }
 }
 
-fn main() -> PyResult<()> {
+fn map_value_to_color(normalized_value: f64) -> u8 {
+    let g = (normalized_value * 255.0) as u8;
+    g
+}
 
+fn visualize_filter(filter: &Filter, real: bool) -> Result<(), Box<dyn std::error::Error>> {
+
+    let filter_to_make;
+
+    if real {
+        filter_to_make = &filter.filter_real;
+    } else {
+        filter_to_make = &filter.filter_imag;
+    }
+
+    let (image_width, image_height) = (filter_to_make.num_columns() as u32, filter_to_make.num_rows() as u32);
+    let mut imgbuf: ImageBuffer<Luma<u8>, Vec<u8>> = ImageBuffer::new(image_height, image_width);
+
+    let max_value = filter_to_make.as_rows().iter().chain(filter.filter_imag.as_rows().iter())
+        .flatten()
+        .cloned()
+        .fold(f64::MIN, f64::max);
+    let min_value = filter_to_make.as_rows().iter().chain(filter.filter_imag.as_rows().iter())
+        .flatten()
+        .cloned()
+        .fold(f64::MAX, f64::min);
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let i = y as usize; // rows
+        let j = x as usize; // columns
+    
+        let value = filter_to_make[(i,j)]; 
+        let normalized_value = (value - min_value) / (max_value - min_value);
+        let color_value =  map_value_to_color(normalized_value);
+    
+        // Set the pixel in the image buffer.
+        *pixel = Luma([color_value]);
+    }
+    
+    imgbuf.save("filter.png").unwrap();
+    Ok(())
+}
+
+fn main() -> PyResult<()> {
 
     Python::with_gil(|py| {
         let sys = PyModule::import(py, "sys")?;
 
         let path: &PyList = sys.getattr("path")?.downcast()?;
-        println!("");
-        println!("Python sys.path: {:?}", path);
 
-        path.insert(0, "../../python_modules")?; // Update this path
+        path.insert(0, "../python_modules")?; // Update this path
 
         let gaussian_module = PyModule::import(py, "gaussian")?;
-        let mu = vec![40, 40];
+        let mu = vec![20, 20];
+        let sigma = 100;
+        let size = 101;
+        let use_log = true;
 
-        let args = (3, mu, 10, true);
+        let args = (size, mu, sigma, use_log);
         
-        let result: Vec<Vec<f64>> = gaussian_module
+        let (result_real, result_imag): (Vec<Vec<f64>>, Vec<Vec<f64>>) = gaussian_module
             .getattr("gaussian")?
             .call1(args)?
             .extract()?;
-        let filter: Filter = Filter::new(result);
+
+        let filter: Filter = Filter::new(result_real, result_imag);
         let mut filters: Vec<Filter> = vec![];
         filters.push(filter);
-        
-        println!("Gaussian result: {:?}", filters.len());
-        println!("Gaussian result: {:?}", filters[0].shape);
-        println!("Gaussian result: {:?}", filters[0]);
 
+        let real = false;
+        let _ = visualize_filter(&filters[0], real);
 
         Ok(())
     })
--- python_modules/gaussian.py
+++ python_modules/gaussian.py
@@ -7,47 +7,27 @@ import numpy as np
 
 #n: np.ndarray, mu: np.ndarray, sigma: float, use_log: bool
 
-def gaussian(n, mu: np.ndarray, sigma, use_log):
+def gaussian(size, mu: np.ndarray, sigma, use_log):
 
     mu = np.array(mu)
-
-    N = 9
-    
-    print("Hello world")
-    
-
-    n1 = np.arange(N).reshape((-1, 1)).repeat(N, axis=1) - (N - 1) / 2
-
-    n2 = n1.T
-    n = np.array([n1, n2])
-
-    print(n)
-        
-    N = n.shape[1]
-
     print(f"dette er mu {mu}")
+    n1 = np.arange(size).reshape((-1, 1)).repeat(size, axis=1) - (size - 1) / 2
+    n2 = n1.T
+    n = np.array([n1, n2])  
 
-
+    N = n.shape[1]
     scale = ((N - 1.) / 2.) / np.log((N + 1) / 2.) if use_log else 1.
 
     def log(x):
         return np.log(x) if use_log else x - 1
 
-    result = np.zeros((N, N))
-    for l1 in [-N, 0, N]:
-        for l2 in [-N, 0, N]:
+    result = np.zeros((size, size))
+    for l1 in [-size, 0, size]:
+        for l2 in [-size, 0, size]:
             norm = np.linalg.norm(n + np.array([l1, l2]).reshape(-1, 1, 1), axis=0)
             result += np.exp(
                 -np.square(scale * np.where(norm > 0, (n + np.array([l1, l2]).reshape(-1, 1, 1)) / norm * log(norm + 1), 0.) - mu.reshape(-1, 1, 1)).sum(axis=0)
                 / sigma)    
 
-     
-    print(f"dette er result {result}")
-    print(f"dette er typen til result {type(result)}")
-    print(f"Dette er shapen til result {result.shape}")
-    return result
-    
-    # if use_log : 
-    #     return sigma * 10
-    # else:
-    #     return sigma/10
+    spatial_domain = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(result)))
+    return (spatial_domain.real, spatial_domain.imag) 
