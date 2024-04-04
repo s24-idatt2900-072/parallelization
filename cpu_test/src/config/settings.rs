6d2296199e59f03f249a8264c2e762218be1cdde
ecschoye <ecschoye@stud.ntnu.no>
2024-04-04 17:11:28 +0200

    update num images

--- cpu_test/src/config/settings.rs
+++ cpu_test/src/config/settings.rs
@@ -1,6 +1,6 @@
 pub const IMAGE_SIZE: u32 = 29;
 pub const SEGMENT_SIZE: usize = 3;
-pub const NUM_IMAGES: u32 = 5;
+pub const NUM_IMAGES: u32 = 50000; // MAX 50000
 pub const OUTPUT_PARALLELL_PATH: &str = "files/output_parallell.csv";
 pub const OUTPUT_SEQUENTIAL_PATH: &str = "files/output_sequential.csv";
 pub const FILTERS_PATH: &str = "files/filters.csv";
