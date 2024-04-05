27a6a3dd1466d0aa448ba6d47815caa009321428
ecschoye <ecschoye@stud.ntnu.no>
2024-04-05 10:13:20 +0200

    added parallelization for finalizing of results

--- wgpu_test/src/main.rs
+++ wgpu_test/src/main.rs
@@ -1,45 +1,73 @@
+use rayon::prelude::*;
+use std::time::Instant;
 use wgpu_test::Extractor;
 
 fn main() {
     // TODO: initiate logger instead of print
     //print_devices();
+    let now = Instant::now();
     test_simple_feature_extraction();
+    let elapsed = now.elapsed();
+    println!("Total elapsed time: {:.2?}", elapsed);
 }
 
 fn test_simple_feature_extraction() {
     // Data for computation
     println!("Initializing data..");
-    let a: Vec<Vec<f32>> = vec![vec![1.; 841]; 3_800];
+    let a: Vec<Vec<f32>> = vec![vec![1.; 841]; 3800];
     let b: Vec<Vec<f32>> = vec![vec![1.; 841]; 100_000];
     // Result buffer
     let mut res: Vec<Vec<f32>> = vec![vec![0.; b.len()]; a.len()];
 
+    let now = Instant::now();
     println!("\nComputing..");
     let ex = Extractor::new().unwrap();
     let flat_output = ex
         //.get_features(&a, &b, chunk, filter_chunk)
-        .dot(&a, &b, (250, 6_250, 1))
+        .dot(&a, &b, (238, 6_250, 1))
         .unwrap();
     //println!("Output: {:?}", flat_output);
     //println!("Output: {:?}", flat_output[100]);
     //println!("Flat output: {:?}", flat_output);
     //write_wgpu_res_to_file(&flat_output).unwrap();
-
+    let elapsed = now.elapsed();
+    println!("Elapsed computing time: {:.2?}", elapsed);
+    
     println!("Finalizing..");
-    let mut it = flat_output.into_iter();
-    let _ = res
-        .iter_mut()
-        .map(|inner| inner.iter_mut().for_each(|r| *r = it.next().unwrap()))
-        .collect::<Vec<_>>();
+    let chunk_sizes = res.iter().map(|inner| inner.len()).collect::<Vec<_>>();
+    let mut starts = vec![0];
+    let mut sum = 0;
+    for size in &chunk_sizes {
+        sum += *size;
+        starts.push(sum);
+    }
 
+    res.par_iter_mut().enumerate().for_each(|(i, inner)| {
+        let start = starts[i];
+        let end = starts[i + 1];
+        let slice = &flat_output[start..end];
+        inner.iter_mut().enumerate().for_each(|(j, r)| *r = slice[j]);
+    });
+    /*
     println!(
-        "Total number of elemnts: {}",
-        res.iter().flatten().collect::<Vec<&f32>>().len()
+        "Total number of elements: {}",
+        res.par_iter().flatten().count()
     );
     println!(
         "Number of elements wrong: {}",
-        res.iter().flatten().filter(|i| i != &&841.).count()
+        res.par_iter().flatten().filter(|i| **i != 841.).count()
     );
+    */
+    let total_elements = res.par_iter().flatten().count();
+    let wrong_elements = res.par_iter().flatten().filter(|i| **i != 841.).count();
+
+    let percentage_wrong = (wrong_elements as f64 / total_elements as f64) * 100.0;
+
+    println!("Total number of elements: {}", total_elements);
+    println!("Number of elements wrong: {}", wrong_elements);
+
+    println!("Percentage of elements wrong: {:.2}%", percentage_wrong);
+
     //println!("Wrong elements:\n{:?}", res.iter().flatten().filter(|i| i != &&841.).collect::<Vec<&f32>>());
     //println!("Numbers wrong: {:?}", res.iter().flatten().filter(|i| i != &&841.).collect::<Vec<&f32>>());
     //println!("indexes of wrong: {:?}", res.iter().enumerate().map(|(i, inner)| inner.iter().enumerate().filter(|(j, r)| r != &&841.).map(|(j, r)| (i, j)).collect::<Vec<(usize, usize)>>()).collect::<Vec<Vec<(usize, usize)>>>());
