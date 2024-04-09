6f6da2460d09d4ca703d343ee4acbaa053b5bb5a
Vetle Ålesve Nordang <vetlean@stud.ntnu.no>
2024-04-09 10:54:22 +0200

    added python script for making filters and writing to  file

--- python_modules/gaussian.py
+++ python_modules/gaussian.py
@@ -1,32 +1,20 @@
 import numpy as np
 
-# n er størrelsen på bilde/filteret som er n*n fylt med verdier fra (n-1)/2 til -(n-1)/2 
-# mu er koordinatene en velger å starte filteret på
-# sigma bestemmer hvor store frekvenser som går gjennom"
-# use_log er om den skal bruke logaritme til utergning eller ikke
+def gaussian(n: np.ndarray, mu: np.ndarray, sigma: float, use_log: bool):
+    N = n.shape[1] # N er 101 
 
-#n: np.ndarray, mu: np.ndarray, sigma: float, use_log: bool
-
-def gaussian(size, mu: np.ndarray, sigma, use_log):
-
-    mu = np.array(mu)
-    n1 = np.arange(size).reshape((-1, 1)).repeat(size, axis=1) - (size - 1) / 2
-    n2 = n1.T
-    n = np.array([n1, n2])  
-
-    N = n.shape[1]
     scale = ((N - 1.) / 2.) / np.log((N + 1) / 2.) if use_log else 1.
 
     def log(x):
         return np.log(x) if use_log else x - 1
 
-    result = np.zeros((size, size))
-    for l1 in [-size, 0, size]:
-        for l2 in [-size, 0, size]:
+    result = np.zeros((N, N))
+    for l1 in [-N, 0, N]:
+        for l2 in [-N, 0, N]:
             norm = np.linalg.norm(n + np.array([l1, l2]).reshape(-1, 1, 1), axis=0)
             result += np.exp(
                 -np.square(scale * np.where(norm > 0, (n + np.array([l1, l2]).reshape(-1, 1, 1)) / norm * log(norm + 1), 0.) - mu.reshape(-1, 1, 1)).sum(axis=0)
-                / sigma)    
-
-    spatial_domain = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(result)))
-    return (spatial_domain.real, spatial_domain.imag) 
+                / sigma)
+    
+    #     spatial_domain = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(result)))
+    return result
