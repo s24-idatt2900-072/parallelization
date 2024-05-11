a4d08481db191bf0c7d7542252201b6d8fea1ea3
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-05-11 12:39:47 +0200

    Python fixed with new and old way

--- python/src/main.py
+++ python/src/main.py
@@ -4,6 +4,7 @@ from pooling import max_pooling
 from file_writer import write_to_file
 import time
 import sys
+import numpy as np
 
 def main():
     initial_ui()
@@ -12,16 +13,26 @@ def research():
     """
     Will do the research run
     start amount of image set by the user
-    start with 20 filters, do 30 runs with those configurations, then increase by 1 and continue indefinitely
+    start with 20 filters, do 60 runs with those configurations, then increase by 1 and continue indefinitely
 
     logging will be done to a csv file
     columns: Filter, ID, Time_ms, Average_time
-    filter will be the filter number for the 30 runs
-    id will be the id of the run, starting at 0 and go on to 29
+    filter will be the filter number for the 60 runs
+    id will be the id of the run, starting at 1 and go on to 60
     time_ms will be the time it took to run the process_images_with_filters function and max_pooling function
-    average_time will be the average time of the 30 runs for that filter amount
+    average_time will be the average time of the 60 runs for that filter amount
     """
     
+    print("Old way or new way?")
+    print("1. Old way")
+    print("2. New way")
+    method = 0
+    while method < 1 or method > 2:
+        print("Enter the method number:")
+        method = int(input())
+        if method < 1 or method > 2:
+            print("Invalid method number.")
+
     print("Enter the amount of images to process:")
     num_images = 0
 
@@ -41,41 +52,56 @@ def research():
     mnist_images = load_data("files/mnist/mnist_padded_29x29.csv")
     print(f"Data loading took {time.time() - start_time} seconds.")
 
-    file_name = f"CPU_img_{num_images}_{int(time.time())}.csv"
+    # file with unique id using unix timestamp
+    file_name = f"platform-X-python-{int(time.time())}.csv"
 
-    filter_amount = 100
+    filter_amount = 10
 
     increment = 10
 
     pool_size = 500
 
-    data_to_write = [("Filter", "ID", "Time_ms", "Average_time")]
+    data_to_write = [("Filter", "ID", "Time_us", "Average_time")]
 
     images_stack = load_and_stack_data(mnist_images, num_images)
 
     while True:
         prev_filter_amount = filter_amount
-        time_ms = []
+        time_us = []
         print(f"Processing with {filter_amount} filters...")
-        for i in range(1, 31):
-            start_time = time.time()
+        for i in range(1, 61):
             abs_filters_stack = load_and_stack_data(abs_filters, filter_amount)
             real_filters_stack = load_and_stack_data(real_filters, filter_amount)
-            processed_results = process_images_with_filters(images_stack, real_filters_stack, abs_filters_stack)
-            max_pooled_results = max_pooling(processed_results, pool_size)
-            time_spent_ms = int(round((time.time() - start_time) * 1000))
-            time_ms.append(time_spent_ms)
-            if i == 1 or filter_amount != prev_filter_amount:
-                data_to_write.append((filter_amount, i, time_spent_ms, 0))
+            
+            start_time = time.time()
+
+            if method == 1:
+                # The Old way
+                processed_results = process_images_with_filters(images_stack, real_filters_stack, abs_filters_stack)
             else:
-                data_to_write.append((0, i, time_spent_ms, 0))
-        average_time = int(round(sum(time_ms) / len(time_ms)))
+                # The New way
+                d = images_stack[:, np.newaxis, :] * abs_filters_stack
+                dot = np.sum(d * real_filters_stack[np.newaxis, :, :], axis=(2,3))
+                norm = np.sum(d * d, axis=(2,3))
+                processed_results = dot / np.sqrt(norm)
+
+            # asserts that the shape of the processed_results is correct numimgaes, num_filters
+            assert processed_results.shape == (num_images, filter_amount)
+            
+            max_pooled_results = max_pooling(processed_results, pool_size)
+            time_spent_us = int(round((time.time() - start_time) * 1000000))
+            time_us.append(time_spent_us)
+            if i == 1 or filter_amount != prev_filter_amount:
+                data_to_write.append((filter_amount, i, time_spent_us, 0))
+            else:
+                data_to_write.append((0, i, time_spent_us, 0))
+        average_time = int(round(sum(time_us) / len(time_us)))
         data_to_write.append((0, 0, 0, average_time))
         write_to_file(file_name, data_to_write)
         data_to_write = []
         filter_amount += increment
         prev_filter_amount = filter_amount
-        if filter_amount > 15000:
+        if filter_amount > 2500:
             break
 
 
